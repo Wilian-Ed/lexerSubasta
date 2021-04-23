b3c8448b35379a565b50ee549f0fc4f706be4f6c
Wilian Soch <55908367+Wilian-Ed@users.noreply.github.com>
2021-04-22 23:38:59 -0600

    Creando proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DescriptionComponent } from './description/description.component';
+import { TextComponent } from './text/text.component';
+import { TableComponent } from './table/table.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DescriptionComponent,
+    TextComponent,
+    TableComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
