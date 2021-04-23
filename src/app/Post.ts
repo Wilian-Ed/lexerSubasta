f130720b81da09463dee60c8cf528789cc6cc7f5
Wilian Soch <55908367+Wilian-Ed@users.noreply.github.com>
2021-04-23 02:48:58 -0600

    add table

---
+++ src/app/Post.ts
@@ -0,0 +1,5 @@
+export interface Post{
+  'Nombre': string;
+  'Valor': string;
+  'Codigo': string;
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {JsonService} from './json.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent{
   title = 'lexerSubasta';
+
+
+  constructor(private json: JsonService){
+    this.json.getJson('http://analizadorlexico1.herokuapp.com/api/table').subscribe((res: any) => {console.log(res); });
+  }
+
+  posts = [];
+
 }
